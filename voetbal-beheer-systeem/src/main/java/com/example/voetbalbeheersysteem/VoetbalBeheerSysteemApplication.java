cf0dbd391908f928fea9904c5c83b18ee9b16447
Alae-M <r0955035@student.thomasmore.be>
2024-12-16 18:40:19 +0100

    Initial commit: project structure added

---
+++ voetbal-beheer-systeem/src/main/java/com/example/voetbalbeheersysteem/VoetbalBeheerSysteemApplication.java
@@ -0,0 +1,13 @@
+package com.example.voetbalbeheersysteem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoetbalBeheerSysteemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoetbalBeheerSysteemApplication.class, args);
+    }
+
+}
---
+++ voetbal-beheer-systeem/src/test/java/com/example/voetbalbeheersysteem/VoetbalBeheerSysteemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voetbalbeheersysteem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoetbalBeheerSysteemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
